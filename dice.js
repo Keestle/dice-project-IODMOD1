5d92f28e6b725f54b739e37b228cf77926697c6e
Keestle <yeeern98@gmail.com>
2023-12-09 22:33:50 +0800

    Problem solved

--- dice.js
+++ dice.js
@@ -3,8 +3,8 @@ function rolld6() {
         return Math.floor(Math.random() * 6) + 1;
 }
 
-result1 = rolld6(6);
+const result1 = rolld6();
 console.log(`Rolling a 6-sided die: ${result1}`);
 
 
-module.exports = {rolld6}
+module.exports = rolld6;
